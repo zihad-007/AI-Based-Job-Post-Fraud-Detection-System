31c56930a788c58866e844502f16cb39f319f464
Rakibul Hasan Zihad <rakibulhasanzihad0502@gmail.com>
2026-03-03 22:03:28 +0600

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 from flask_bcrypt import Bcrypt
 
@@ -7,7 +7,7 @@ app.secret_key = "secretkey123"
 
 bcrypt = Bcrypt(app)
 
-# create database
+
 def init_db():
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
@@ -27,16 +27,20 @@ def init_db():
 init_db()
 
 
-# Registration
+@app.route("/")
+def home():
+    return redirect("/login")
+
+
 @app.route("/register", methods=["GET","POST"])
 def register():
 
     if request.method == "POST":
+
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
 
-        # validation
         if len(password) < 6:
             return "Password must be at least 6 characters"
 
@@ -46,23 +50,26 @@ def register():
         cur = conn.cursor()
 
         try:
-            cur.execute("INSERT INTO users(username,email,password) VALUES(?,?,?)",
-                        (username,email,hashed_pw))
+            cur.execute(
+                "INSERT INTO users(username,email,password) VALUES(?,?,?)",
+                (username,email,hashed_pw)
+            )
             conn.commit()
         except:
             return "User already exists"
 
         conn.close()
-        return redirect("/login")
+
+        return redirect(url_for("login"))
 
     return render_template("register.html")
 
 
-# Login
 @app.route("/login", methods=["GET","POST"])
 def login():
 
     if request.method == "POST":
+
         username = request.form["username"]
         password = request.form["password"]
 
@@ -75,15 +82,16 @@ def login():
         conn.close()
 
         if user and bcrypt.check_password_hash(user[3], password):
+
             session["user"] = username
             return "Login Successful"
+
         else:
             return "Invalid username or password"
 
     return render_template("login.html")
 
 
-# Logout
 @app.route("/logout")
 def logout():
     session.pop("user", None)
