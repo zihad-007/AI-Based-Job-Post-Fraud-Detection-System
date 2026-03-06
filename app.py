26685d48ecb650fc98a4386f763e0ef16ea180ac
TOKY YASIR <tokyyasir55628@gmail.com>
2026-03-06 21:42:52 +0600

    Update app.py

--- app.py
+++ app.py
@@ -154,36 +154,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True) import sqlite3
-
-conn = sqlite3.connect("database.db")
-
-cursor = conn.cursor()
-
-# users table
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT,
-contact TEXT,
-password TEXT
-)
-""")
-
-# job posts table
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS job_posts(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-company TEXT,
-description TEXT,
-salary TEXT,
-type TEXT,
-result TEXT
-)
-""")
-
-conn.commit()
-conn.close()
-
-print("Database created successfully")
+    app.run(debug=True)
